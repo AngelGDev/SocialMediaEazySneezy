be7bcb0cde0a674d0bd1d4bac50d0eab98cfd55d
Alex Deatherage <alexdeatherage4829@gmail.com>
2021-04-27 18:21:28 -0700

    Add comments for controllers folder

--- controllers/home.js
+++ controllers/home.js
@@ -1,3 +1,5 @@
+// Need to render the index.ejs file as html for the user.
+// Likely need to expand as we need to render more files.
 module.exports = {
     getIndex: (req,res)=>{
         res.render('index.ejs')
--- controllers/todos.js
+++ controllers/todos.js
@@ -1,6 +1,10 @@
 const Todo = require('../models/Todo')
 
+// Likely going to need to delete, as we need to make new controllers for
+// the posting screen and also the feed screen.
 module.exports = {
+
+    // we would need a get for getting the posts from other users.
     getTodos: async (req,res)=>{
         console.log(req.user)
         try{
@@ -13,6 +17,7 @@ module.exports = {
             console.log(err)
         }
     },
+    // need a create for making posts to the feed. 
     createTodo: async (req, res)=>{
         try{
             await Todo.create({todo: req.body.todoItem, completed: false, microsoftId: req.user.microsoftId})
@@ -22,6 +27,7 @@ module.exports = {
             console.log(err)
         }
     },
+    // Not likely needed, as we don't need to mark items as complete.
     markComplete: async (req, res)=>{
         try{
             await Todo.findOneAndUpdate({_id:req.body.todoIdFromJSFile},{
@@ -33,6 +39,7 @@ module.exports = {
             console.log(err)
         }
     },
+    // Not likely needed, as we don't need to mark items as complete.
     markIncomplete: async (req, res)=>{
         try{
             await Todo.findOneAndUpdate({_id:req.body.todoIdFromJSFile},{
@@ -44,6 +51,8 @@ module.exports = {
             console.log(err)
         }
     },
+    
+    // Would likely need, depending on if we allow users to be able to delete their posts or not.
     deleteTodo: async (req, res)=>{
         console.log(req.body.todoIdFromJSFile)
         try{
