a460d709333e23c6a80668bdf201f9c8f4e26216
jgodinez <jgodinez1114@gmail.com>
2021-04-29 15:35:05 -0500

    added logic to main.js and style.css in Public folder

---
+++ public/js/main.js
@@ -0,0 +1,61 @@
+/******************************************************
+*
+* Programmer: Joel Godinez 
+* 
+* Date: April 28, 2021
+*
+* Files included: /public/js/main.js
+*
+* Environment: node.js
+*
+* Purpose: update 'likes', show posts, show user home feed, display option to make a 'post', 
+*          display option to 'show messages', display option to 'show and add comments' 
+*
+* Input: user click events, text input on comments
+*
+* Preconditions/ Assumptions: data displayed on screen is the most up to date
+*
+* Output: like counter update, new comments, show newest home feed on every click of Home button, 
+*         show messages on message buton click, show the newest data on a 'page refresh'
+*
+* Postconditions: deleted posts will no longer appear on a page 'refresh', user can only view their own messges
+*
+* Algorithm:
+*	1 Create variable to store user click input per button class
+*	2 Create an event listener per button class
+*	3 Call method to carry out a specific task per button class
+*	4 Output on screen data (i.e. total likes on a post, remove deleted posts)
+*
+*	 
+*
+*    Estimates                  Actuals
+*     Design: 1 hour            x hours
+*     Implementation: 4 hours   x hours
+*     Testing:        2 hours   x hours
+*
+*
+**************************************************************************/
+
+// Go to Home feed ".home" 
+
+// Show New posts "span.newPost"
+
+// Remove Deleted posts "span.deletedPost"
+
+// Show likes on a Post ".likes"
+
+// add event listerner for homeBtn
+
+// add event listener for 'deletedPost'
+
+// add event listener for 'likesBtn'
+
+// add event listener for 'addCommentBtn'
+
+// add event listener for 'postPicBtn'
+
+// add event listener for 'goHomeBtn'
+
+// add event listener for 'viewMessagesBtn'
+
+// add event listener for 'seeProfileBtn'
