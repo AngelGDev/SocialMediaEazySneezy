1977552a77fcb4dce45e89cc1c07f0ec9426b52d
laylar <layla.rowen@gmail.com>
2021-04-29 17:13:12 -0700

    #23 Updated User Schema with hash middleware

--- models/User.js
+++ models/User.js
@@ -1,53 +1,50 @@
 const mongoose = require("mongoose");
-// REQUIRE Middleware to hash passwords
-// const bcrypt = require('bcrypt')
+// Middleware to hash passwords
+const bcrypt = require("bcrypt");
 
 const UserSchema = new mongoose.Schema({
-  // userId instead of microsoftId
-  microsoftId: {
+  userId: {
     type: String,
     required: true,
   },
-  // password:{
-  //  type: String,
-  //  required: true,
-  // }
-
-  // email: {
-  //type: String,
-  //required: true
-  //  }
-
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
   displayName: {
     type: String,
     required: true,
   },
 });
 
-// ADD UserScheme Middleware to HASH PASSWORDS
+// UserScheme Middleware to HASH PASSWORDS
 // https://www.twitch.tv/videos/1000589317?t=02h50m22s
 // copied from https://www.twitch.tv/videos/1000589317?t=02h51m41s
 //----------------------------------------------------------------
-// UserSchema.pre('save', function save(next)) {
-//   const user = this
-//   if (!user.isModified('password')) { return next()}
-//   bcrypt.genSalt(10, (err, salt) => {
-//     if (err) { return next(err) }
-//     bcrypt.hash(user.password, salt, (err, hash) => {
-//       if(err) { return next(err)}
-//       user.password = hash
-//       next()
-//     })
-//   })
-// }
+UserSchema.pre('save', function save(next)) {
+  const user = this
+  if (!user.isModified('password')) { return next()}
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) { return next(err) }
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if(err) { return next(err)}
+      user.password = hash
+      next()
+    })
+  })
+}
 
-// ADD Helper Method to Compare Passwords
+// Helper Method to Compare Passwords
 // copied from https://www.twitch.tv/videos/1000589317?t=02h51m44s
 //----------------------------------------------------------------
-// UserSchema.methods.comparePassword = function comparePassword(candidatePassword, cb) {
-//   bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
-//     cb(err, isMatch)
-//   })
-// }
+UserSchema.methods.comparePassword = function comparePassword(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    cb(err, isMatch)
+  })
+}
 
 module.exports = mongoose.model("User", UserSchema);
